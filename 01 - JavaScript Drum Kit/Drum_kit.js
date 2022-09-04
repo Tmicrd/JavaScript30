16341f93d68134f4ef5b570fb0a475a2df92cd65
nancy <liyingxiong23@gmail.com>
2022-09-04 15:50:13 +1000

    Updated Day1

---
+++ 01 - JavaScript Drum Kit/Drum_kit.js
@@ -0,0 +1,23 @@
+window.addEventListener("keydown", (e) => {
+  
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+  console.log(key);
+
+  function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    key.classList.remove("playing");
+    console.log(e.propertyName);
+  }
+
+  const keys = document.querySelectorAll(".key");
+
+  keys.forEach((key) => {
+    key.addEventListener("transitionend", removeTransition);
+  });
+});
